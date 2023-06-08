9ca63202b01811304f4ce64971d2a09ffd19b316
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-06-08 22:41:47 +0600

    Create .env , config ,install ts-node-dev, connect to atlas

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+// getting-started.js
+import mongoose from "mongoose";
+import app from './app'
+import config from "./config";
+async function bootstrap() {
+    try {
+        await mongoose.connect(config.database_url as string);
+        console.log('Database is connected Successfully')
+
+        app.listen(config.port, () => {
+            console.log(`Application listening on port ${config.port}`)
+        })
+    }
+    catch (err) {
+        console.log('Something is wrong!, Failed to connect DB', err)
+    }
+}
+
+bootstrap();
