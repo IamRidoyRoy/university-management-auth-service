09becbecd86dec4a231a56a63b74e4cedca2bfac
Ridoy Roy <ridoyroycse53engg@gmail.com>
2023-06-10 00:31:00 +0600

    Update package.json

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,15 @@
 import express, { Application, urlencoded, Request, Response } from 'express'
 import cors from 'cors'
-const app: Application = express();
-const port = 3000
+const app: Application = express()
 
-// parser 
-app.use(express.json());
-app.use(urlencoded({ extended: true }));
+// parser
+app.use(express.json())
+app.use(urlencoded({ extended: true }))
 
 // Testing
-app.use(cors());
+app.use(cors())
 app.get('/', (req: Request, res: Response) => {
-    res.send('Working Successfully')
+  res.send('Working Successfully')
 })
 
-export default app;
+export default app
